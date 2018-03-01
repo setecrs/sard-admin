3578937ea523c3eb6243d71c6444d718ec048323
Atila Romero <atilaromero@gmail.com>
2018-03-01 08:46:01 -0300

    IPED-SearchApp.exe

--- sardcmd.py
+++ sardcmd.py
@@ -114,6 +114,7 @@ class Operacao:
     def permissoes(self):
         op=self.name
         command('find /operacoes/"%s" -name indexador -prune -o -name "Ferramenta de Pesquisa.exe" -print0 | xargs -L1 -0 chmod -v a+x '%(op))
+        command('find /operacoes/"%s" -name indexador -prune -o -name "IPED-SearchApp.exe" -print0 | xargs -L1 -0 chmod -v a+x '%(op))
         command('chown -vR -h root:"%s" /operacoes/"%s"'%(op,op))
         command('chmod -v  u+rX,g+rX,o-rwx /operacoes/"%s"'%(op))
         command('chmod -vR a+rX            /operacoes/"%s"/*'%(op))
