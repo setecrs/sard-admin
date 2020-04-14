30ea9f89c7ab642e22acba3ff72514c6f15d0811
Atila Romero <atilaromero@gmail.com>
2020-04-14 16:32:38 -0300

    version in setup.py

--- sard-admin/setup.py
+++ sard-admin/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='sardadmin',
-    version='2.0.3',
+    version='2.0.5',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
