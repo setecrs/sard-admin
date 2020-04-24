b95190f212202a17e4f057b8cb51945eb4e5d892
Atila Romero <atilaromero@gmail.com>
2020-04-24 13:39:43 -0300

    encapsulate query

--- sard-admin-ui/src/data/card_fetcher.ts
+++ sard-admin-ui/src/data/card_fetcher.ts
@@ -25,7 +25,8 @@ export function CardFetcher({ graphqlURL }: { graphqlURL: string }): CardFetcher
         listProcessing: async () => {
             const req = new Request(graphqlURL, {
                 method: "POST",
-                body: listProcessingQuery,
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ query: listProcessingQuery }),
             })
             const resp = await fetch(req)
             return listProcessingCore(resp)
@@ -53,13 +54,13 @@ export async function listProcessingCore(resp: Response): Promise<ProcessingCard
     }
     const j = await resp.json()
     const { cards } = j.data.board.list
-    const result = cards.map((x:any) => {
+    const result = cards.map((x: any) => {
         const p: ProcessingCard = {
             id: x.title,
             properties: {},
         }
         if (x.customFields) {
-            x.customFields.forEach((c:{
+            x.customFields.forEach((c: {
                 customField: {
                     name: keyof ProcessingCard["properties"]
                 },
@@ -76,61 +77,73 @@ export async function listProcessingCore(resp: Response): Promise<ProcessingCard
 export function MockCardFetcher(): CardFetcherType {
     return {
         listProcessing: async () => {
-            const x:ProcessingCard[] = [
-                {id: "M190001", properties:{
-                    auto:"",
-                    ipl:"ipl_180001",
-                    item:"1",
-                    path:"/operacoes/ipl_180001/item01-M190001/item01-M190001.dd",
-                    profile:"",
-                    registro:"R190020",
-                    status:"done",
-                }},
-                {id: "M190002", properties:{
-                    auto:"",
-                    ipl:"ipl_180001",
-                    item:"1",
-                    path:"/operacoes/ipl_180001/item01-M190002/item01-M190002.dd",
-                    profile:"",
-                    registro:"R190020",
-                    status:"done",
-                }},
-                {id: "M190003", properties:{
-                    auto:"",
-                    ipl:"ipl_180001",
-                    item:"1",
-                    path:"/operacoes/ipl_180001/item01-M190003/item01-M190003.dd",
-                    profile:"",
-                    registro:"R190020",
-                    status:"failed",
-                }},
-                {id: "M190004", properties:{
-                    auto:"",
-                    ipl:"ipl_180001",
-                    item:"1",
-                    path:"/operacoes/ipl_180001/item01-M190004/item01-M190004.dd",
-                    profile:"",
-                    registro:"R190020",
-                    status:"",
-                }},
-                {id: "M190005", properties:{
-                    auto:"",
-                    ipl:"ipl_180001",
-                    item:"1",
-                    path:"/operacoes/ipl_180001/item01-M190005/item01-M190005.dd",
-                    profile:"",
-                    registro:"R190020",
-                    status:"running",
-                }},
-                {id: "M190006", properties:{
-                    auto:"",
-                    ipl:"ipl_180001",
-                    item:"1",
-                    path:"/operacoes/ipl_180001/item01-M190006/item01-M190006.dd",
-                    profile:"",
-                    registro:"R190020",
-                    status:"running",
-                }},
+            const x: ProcessingCard[] = [
+                {
+                    id: "M190001", properties: {
+                        auto: "",
+                        ipl: "ipl_180001",
+                        item: "1",
+                        path: "/operacoes/ipl_180001/item01-M190001/item01-M190001.dd",
+                        profile: "",
+                        registro: "R190020",
+                        status: "done",
+                    }
+                },
+                {
+                    id: "M190002", properties: {
+                        auto: "",
+                        ipl: "ipl_180001",
+                        item: "1",
+                        path: "/operacoes/ipl_180001/item01-M190002/item01-M190002.dd",
+                        profile: "",
+                        registro: "R190020",
+                        status: "done",
+                    }
+                },
+                {
+                    id: "M190003", properties: {
+                        auto: "",
+                        ipl: "ipl_180001",
+                        item: "1",
+                        path: "/operacoes/ipl_180001/item01-M190003/item01-M190003.dd",
+                        profile: "",
+                        registro: "R190020",
+                        status: "failed",
+                    }
+                },
+                {
+                    id: "M190004", properties: {
+                        auto: "",
+                        ipl: "ipl_180001",
+                        item: "1",
+                        path: "/operacoes/ipl_180001/item01-M190004/item01-M190004.dd",
+                        profile: "",
+                        registro: "R190020",
+                        status: "",
+                    }
+                },
+                {
+                    id: "M190005", properties: {
+                        auto: "",
+                        ipl: "ipl_180001",
+                        item: "1",
+                        path: "/operacoes/ipl_180001/item01-M190005/item01-M190005.dd",
+                        profile: "",
+                        registro: "R190020",
+                        status: "running",
+                    }
+                },
+                {
+                    id: "M190006", properties: {
+                        auto: "",
+                        ipl: "ipl_180001",
+                        item: "1",
+                        path: "/operacoes/ipl_180001/item01-M190006/item01-M190006.dd",
+                        profile: "",
+                        registro: "R190020",
+                        status: "running",
+                    }
+                },
             ]
             return x
         }
