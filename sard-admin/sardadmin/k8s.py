d6da216d78516004d0558ae352f20775658b80f5
Atila Romero <atilaromero@gmail.com>
2020-04-28 08:58:26 -0300

    autoformat

--- sard-admin/sardadmin/k8s.py
+++ sard-admin/sardadmin/k8s.py
@@ -13,45 +13,51 @@ IPEDWorker = namedtuple('IPEDWorker', [
     'image'
 ])
 
+
 class K8s:
     def __init__(self, namespace="default"):
-        self.namespace=namespace
+        self.namespace = namespace
         config.load_kube_config()
+
     def listWorkers(self) -> List[IPEDWorker]:
         resp = client.CoreV1Api().list_namespaced_pod(self.namespace)
         return _listWorkers(resp)
+
 
 def getEvidence(pod_ip, pod_port=80) -> str:
     resp = requests.get(f"http://{pod_ip}:{pod_port}/metrics")
     return _getEvidence(resp)
 
+
 class MetricsException(Exception):
     pass
 
-def _getEvidence(resp:requests.Response) -> str:
+
+def _getEvidence(resp: requests.Response) -> str:
     if (not resp.ok):
-        raise MetricsException("Could not get metrics: "+ resp.text)
+        raise MetricsException("Could not get metrics: " + resp.text)
     for family in text_string_to_metric_families(resp.text):
         for sample in family.samples:
-            if sample.name=='ipedworker_runIped_running':
+            if sample.name == 'ipedworker_runIped_running':
                 return sample.labels['evidence']
     return ''
 
-def _listWorkers(resp:client.V1PodList) -> List[IPEDWorker]:
-    result:List[IPEDWorker] = []
+
+def _listWorkers(resp: client.V1PodList) -> List[IPEDWorker]:
+    result: List[IPEDWorker] = []
     item = client.V1Pod()
     for item in resp.items:
         name = item.metadata.name
         if not name.startswith('ipedworker'):
             continue
-        status:client.V1PodStatus = item.status
+        status: client.V1PodStatus = item.status
         pod_ip = status.pod_ip
         host_ip = status.host_ip
         node_name = item.spec.node_name
         ready = False
         image = ''
         if len(status.container_statuses) == 1:
-            container_status:client.V1ContainerStatus = status.container_statuses[0]
+            container_status: client.V1ContainerStatus = status.container_statuses[0]
             ready = container_status.ready
             image = container_status.image
         worker = IPEDWorker(
@@ -64,4 +70,3 @@ def _listWorkers(resp:client.V1PodList) -> List[IPEDWorker]:
         )
         result.append(worker)
     return result
-
