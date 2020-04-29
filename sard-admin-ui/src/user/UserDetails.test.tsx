f8eccd5da7711c7da0bb6497e0cd4ee870b9baf1
Atila Romero <atilaromero@gmail.com>
2020-04-29 16:00:04 -0300

    await wait

--- sard-admin-ui/src/user/UserDetails.test.tsx
+++ sard-admin-ui/src/user/UserDetails.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent, wait } from '@testing-library/react'
 import { UserDetails } from './UserDetails'
 
 describe('list my groups: /user/{u}', () => {
@@ -15,10 +15,12 @@ describe('list my groups: /user/{u}', () => {
             key='myid'
         />
         const { getAllByRole } = render(userDetails)
-        const options = getAllByRole('option')
-        expect(options).toHaveLength(2)
-        expect(options[0].textContent).toBe('')
-        expect(options[1].textContent).toBe('notmine')
+        await wait(() => {
+            const options = getAllByRole('option')
+            expect(options).toHaveLength(2)
+            expect(options[0].textContent).toBe('')
+            expect(options[1].textContent).toBe('notmine')
+        })
         const li = getAllByRole('listitem')
         expect(li).toHaveLength(1)
         expect(li[0].textContent).toBe('mine')
@@ -44,14 +46,16 @@ test('add user to group: /user/{u}/group/{g}', async () => {
         key='myid'
     />
     const { getByRole } = render(userDetails)
-    const select = getByRole('combobox')
-    fireEvent.change(select, { target: { value: 'notmine' } })
-    fireEvent.click(getByRole('button', { name: /add group/i }))
-    expect(called).toBe(1)
-    expect(called_user).toBe('user')
-    expect(called_group).toBe('notmine')
-    fireEvent.click(getByRole('button', { name: /add group/i }))
-    expect(called).toBe(2)
+    await wait(() => {
+        const select = getByRole('combobox')
+        fireEvent.change(select, { target: { value: 'notmine' } })
+        fireEvent.click(getByRole('button', { name: /add group/i }))
+        expect(called).toBe(1)
+        expect(called_user).toBe('user')
+        expect(called_group).toBe('notmine')
+        fireEvent.click(getByRole('button', { name: /add group/i }))
+        expect(called).toBe(2)
+    })
 })
 
 test('fix home: /user/{u}/home', async () => {
@@ -71,9 +75,11 @@ test('fix home: /user/{u}/home', async () => {
         key='myid'
     />
     const { getByRole } = render(userDetails)
-    fireEvent.click(getByRole('button', { name: /fill home directory/i }))
-    expect(called).toBe(1)
-    expect(called_user).toBe('user')
+    await wait(() => {
+        fireEvent.click(getByRole('button', { name: /fill home directory/i }))
+        expect(called).toBe(1)
+        expect(called_user).toBe('user')
+    })
 })
 
 test('permissions: /user/{u}/permissions', async () => {
@@ -93,9 +99,11 @@ test('permissions: /user/{u}/permissions', async () => {
         key='myid'
     />
     const { getByRole } = render(userDetails)
-    fireEvent.click(getByRole('button', { name: /fix home directory permissions/i }))
-    expect(called).toBe(1)
-    expect(called_user).toBe('user')
+    await wait(() => {
+        fireEvent.click(getByRole('button', { name: /fix home directory permissions/i }))
+        expect(called).toBe(1)
+        expect(called_user).toBe('user')
+    })
 })
 test.skip('set password: /user/{u}/reset_password', async () => {
     const userDetails = <UserDetails
@@ -109,6 +117,7 @@ test.skip('set password: /user/{u}/reset_password', async () => {
         key='myid'
     />
     const { getByRole } = render(userDetails)
-
-    expect(getByRole('button', { name: /change password/i })).toBeDefined()
+    await wait(() => {
+        expect(getByRole('button', { name: /change password/i })).toBeDefined()
+    })
 })
