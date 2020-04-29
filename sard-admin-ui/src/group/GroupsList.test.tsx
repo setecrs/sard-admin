8add994c55f0ecd151d2a690196231914745bb21
Atila Romero <atilaromero@gmail.com>
2020-04-29 14:25:02 -0300

    group list filter

--- sard-admin-ui/src/group/GroupsList.test.tsx
+++ sard-admin-ui/src/group/GroupsList.test.tsx
@@ -1,27 +1,57 @@
-import { render } from '@testing-library/react';
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react';
 import { GroupsList } from './GroupsList';
 
 
 test('GroupsList renders', () => {
-    const groupList = GroupsList({
-        groups: [],
-        selectedGroup: 'g1',
-        setSelectedGroup: async () => { },
-    })
+    const groupList = <GroupsList
+        groups={[]}
+        selectedGroup={'g1'}
+        setSelectedGroup={async () => { }}
+    />
     const { baseElement } = render(groupList);
     expect(baseElement).toBeDefined()
 });
 
 test('GroupList has 2 groups, sorted', () => {
-    const groupList = GroupsList({
-        groups: ['g2', 'g1'],
-        selectedGroup: 'g1',
-        setSelectedGroup: async () => { },
-    })
+    const groupList = <GroupsList
+        groups={['g2', 'g1']}
+        selectedGroup={'g1'}
+        setSelectedGroup={async () => { }}
+    />
     const { baseElement } = render(groupList);
     const ul = baseElement.querySelector('#group_list')
     expect(ul).toBeDefined()
     expect(ul.childNodes).toHaveLength(2)
     expect(ul.childNodes[0].textContent).toBe('g1')
     expect(ul.childNodes[1].textContent).toBe('g2')
+});
+
+test('groupList uses a filter', () => {
+    const userList = <GroupsList
+        groups={['alpha', 'beta']}
+        selectedGroup={'group1'}
+        setSelectedGroup={async () => { }}
+    />
+    const { baseElement, getByPlaceholderText } = render(userList);
+    const ul = baseElement.querySelector('#group_list')
+    expect(ul).toBeDefined()
+    expect(ul.childNodes).toHaveLength(2)
+    expect(ul.childNodes[0].textContent).toBe('alpha')
+    expect(ul.childNodes[1].textContent).toBe('beta')
+
+    const filter = getByPlaceholderText('Filter')
+
+    fireEvent.change(filter, { target: { value: 'et' } })
+    expect(ul.childNodes).toHaveLength(1)
+    expect(ul.childNodes[0].textContent).toBe('beta')
+
+    fireEvent.change(filter, { target: { value: 'lph' } })
+    expect(ul.childNodes).toHaveLength(1)
+    expect(ul.childNodes[0].textContent).toBe('alpha')
+
+    fireEvent.change(filter, { target: { value: '' } })
+    expect(ul.childNodes).toHaveLength(2)
+    expect(ul.childNodes[0].textContent).toBe('alpha')
+    expect(ul.childNodes[1].textContent).toBe('beta')
 });
--- sard-admin-ui/src/group/GroupsList.tsx
+++ sard-admin-ui/src/group/GroupsList.tsx
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 
 export function GroupsList({
     groups,
     selectedGroup,
     setSelectedGroup,
-}:{
-    groups:string[],
-    selectedGroup:string,
-    setSelectedGroup:(x:string)=>void,
+}: {
+    groups: string[],
+    selectedGroup: string,
+    setSelectedGroup: (x: string) => void,
 }) {
-    return <ul id='group_list' className="list-group">
-        {groups.sort().map((u, i) => (
-            <li key={i}
-                className={(u === selectedGroup) ? "list-group-item active" : "list-group-item"}
-                onClick={() => setSelectedGroup(u)}>
-                {u}
-            </li>
-        ))}
-    </ul>
+    const [filter, setFilter] = useState('')
+    const [filteredGroups, setFilteredGroups] = useState<string[]>([])
+
+    useEffect(() => {
+        setFilteredGroups([...groups].filter(x => x.indexOf(filter) > -1))
+    }, [filter, groups])
+
+    return <Fragment>
+        Filter: <input
+            placeholder="Filter"
+            value={filter}
+            onChange={e => setFilter(e.target.value)}
+        />
+        <ul id='group_list' className="list-group">
+            {filteredGroups.sort().map((u, i) => (
+                <li key={i}
+                    className={(u === selectedGroup) ? "list-group-item active" : "list-group-item"}
+                    onClick={() => setSelectedGroup(u)}>
+                    {u}
+                </li>
+            ))}
+        </ul>
+    </Fragment>
 }
