85c780f85c36643c3822c8139f7b371ce73ea92e
Atila Romero <atilaromero@gmail.com>
2020-06-24 18:02:45 -0300

    WIP - folder permissions

--- sard-admin/sardadmin/folders.py
+++ sard-admin/sardadmin/folders.py
@@ -30,3 +30,71 @@ def rename_sard_folder(imagepath):
         newsardfolder = nextFolder(newsardfolder)
     os.rename(sardfolder, newsardfolder)
     return True
+
+def listSubfolders(group, subpath=''):
+    xpath = f'/operacoes/{group}/{subpath}'
+    xpath = xpath.rstrip('/')
+    assert xpath == os.path.abspath(xpath)
+    content = os.listdir(xpath)
+    result = set()
+    for x in content:
+        fpath = os.path.join(xpath, x)
+        if os.path.islink(fpath):
+            continue
+        if os.path.isfile(fpath):
+            continue
+        if os.path.isdir(fpath):
+            result.add(x)
+    return sorted(result)
+
+def default_rw_mode(xpath, isDir=None):
+    if isDir is None:
+        isDir = os.path.isdir(xpath)
+    return default_ro_mode(xpath, isDir) + 0o020
+
+def default_ro_mode(xpath, isDir=None):
+    if isDir is None:
+        isDir = os.path.isdir(xpath)
+    xpath = xpath.rstrip('/')
+    assert xpath == os.path.abspath(xpath)
+    if isDir:
+        return 0o050
+    if 'indexador/tools' in xpath:
+        return 0o050
+    if 'indexador/jre/bin' in xpath:
+        return 0o050
+    if 'indexador/lib' in xpath:
+        return 0o050
+    if xpath.endswith('.exe'):
+        return 0o050
+    return 0o040
+
+def check_rw(xpath):
+    xpath = xpath.rstrip('/')
+    assert xpath == os.path.abspath(xpath)
+    stat :os.stat_result = os.stat(xpath)
+    return (stat.st_mode & 0o777) == default_rw_mode(xpath, os.path.isdir(xpath))
+
+def check_ro(xpath):
+    xpath = xpath.rstrip('/')
+    assert xpath == os.path.abspath(xpath)
+    stat :os.stat_result = os.stat(xpath)
+    return (stat.st_mode & 0o777) == default_ro_mode(xpath, os.path.isdir(xpath))
+
+def set_rw(xpath):
+    if not check_rw(xpath):
+        os.chmod(xpath, default_rw_mode(xpath))
+
+def set_ro(xpath):
+    if not check_ro(xpath):
+        os.chmod(xpath, default_ro_mode(xpath))
+
+def check_owner(xpath, gid):
+    xpath = xpath.rstrip('/')
+    assert xpath == os.path.abspath(xpath)
+    stat :os.stat_result = os.stat(xpath)
+    return stat.st_gid == gid and stat.st_uid == 0
+
+def set_owner(xpath, gid):
+    if not check_owner(xpath):
+        os.chown(xpath, 0, gid)
--- sard-admin/sardadmin/test_folders.py
+++ sard-admin/sardadmin/test_folders.py
@@ -1,7 +1,7 @@
 import os
 from pyfakefs.fake_filesystem_unittest import TestCase
 
-from sardadmin.folders import count_sard_folders, rename_sard_folder
+from sardadmin.folders import count_sard_folders, rename_sard_folder, listSubfolders, default_rw_mode, default_ro_mode, set_ro, set_rw
 
 
 class FoldersTest(TestCase):
@@ -49,3 +49,46 @@ class FoldersTest(TestCase):
         self.assertEqual(os.path.exists('/a/b/c/SARD.old2'), True)
         self.assertEqual(os.path.exists('/a/b/c/SARD.old3'), True)
         
+    def test_listsubfolders(self):
+        filepath = '/operacoes/groupfolder/a/b/c/image.dd'
+        self.fs.create_file(filepath)
+        filepath = '/operacoes/groupfolder/f1.txt'
+        self.fs.create_file(filepath)
+        os.symlink('f1.txt', '/operacoes/groupfolder/f2.txt')
+        self.assertTrue(os.path.exists('/operacoes/groupfolder/a/b/c/image.dd'))
+        self.assertTrue(os.path.exists('/operacoes/groupfolder/f1.txt'))
+        self.assertTrue(os.path.exists('/operacoes/groupfolder/f2.txt'))
+        got = listSubfolders('groupfolder')
+        self.assertListEqual(got, ['a'])
+        self.assertListEqual(listSubfolders('groupfolder', 'a'), ['b'])
+        with self.assertRaises(AssertionError):
+            listSubfolders('groupfolder', '/a')
+        self.assertListEqual(listSubfolders('groupfolder', 'a/b'), ['c'])
+        self.assertListEqual(listSubfolders('groupfolder', 'a/b/c'), [])
+        with self.assertRaises(AssertionError):
+            listSubfolders('groupfolder', 'a/..')
+
+    def test_default_rw(self):
+        isDir=True
+        isFile=False
+        self.assertEqual(default_rw_mode('/rw_mode/', isDir), 0o070)
+        self.assertEqual(default_rw_mode('/rw_mode/file.txt', isFile), 0o060)
+        self.assertEqual(default_rw_mode('/rw_mode/a', isDir), 0o070)
+        self.assertEqual(default_rw_mode('/rw_mode/a/file.txt', isFile), 0o060)
+        self.assertEqual(default_rw_mode('/rw_mode/indexador/tools/file', isFile), 0o070)
+        self.assertEqual(default_rw_mode('/rw_mode/indexador/jre/bin/file', isFile), 0o070)
+        self.assertEqual(default_rw_mode('/rw_mode/indexador/lib/file', isFile), 0o070)
+        self.assertEqual(default_rw_mode('/rw_mode/a/file.exe', isFile), 0o070)
+
+    def test_default_ro(self):
+        isDir=True
+        isFile=False
+        self.assertEqual(default_ro_mode('/ro_mode/', isDir), 0o050)
+        self.assertEqual(default_ro_mode('/ro_mode/file.txt', isFile), 0o040)
+        self.assertEqual(default_ro_mode('/ro_mode/a', isDir), 0o050)
+        self.assertEqual(default_ro_mode('/ro_mode/a/file.txt', isFile), 0o040)
+        self.assertEqual(default_ro_mode('/ro_mode/indexador/tools/file', isFile), 0o050)
+        self.assertEqual(default_ro_mode('/ro_mode/indexador/jre/bin/file', isFile), 0o050)
+        self.assertEqual(default_ro_mode('/ro_mode/indexador/lib/file', isFile), 0o050)
+        self.assertEqual(default_ro_mode('/ro_mode/a/file.exe', isFile), 0o050)
+
